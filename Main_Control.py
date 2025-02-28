cdd0b7793dbd9b03d1d047c471bd65b29d514176
Edgar Francisco Arechiga Nuño <122136427+Sesgaro@users.noreply.github.com>
2025-02-27 18:09:41 -0600

    Update Main_Control.py

--- Main_Control.py
+++ Main_Control.py
@@ -1,9 +1,8 @@
-import Pwm_and_control_Test1.py as motors
-import serial
+import Pwm_and_control_Test1 as motors
 import time
 from inputs import get_gamepad
 
-ESPvalue = espDefine('COM8')
+ESPvalue = motors.esp_define('COM8')
 
 motorFactors = {
     "M1": 100,
@@ -12,20 +11,24 @@ motorFactors = {
     "M4": 120,
 }
 
+trigger_left = 0
+trigger_right = 0
+last_data = ''
+
 while True:
-    
+    events = get_gamepad()
     for event in events:
+        if event.code == "ABS_Z":
+            trigger_left = event.state
         
-      if event.code == "ABS_Z":
-          trigger_left = event.state
+        elif event.code == "ABS_RZ":
+            trigger_right = event.state
         
-      elif event.code == "ABS_RZ":
-          trigger_right = event.state
+        elif event.code == "ABS_X":
+            joy1 = event.state
           
-      elif event.code == "ABS_X":
-          joy1 = event.state
+        elif event.code == "ABS_RX":
+            joy2 = event.state
+    # motors.read_data(ESPvalue)
           
-      elif event.code == "ABS_RX":
-          joy2 = event.state
-          
-    espMagic(ESPvalue, motorFactors, trigger_left, trigger_right)
+    last_data = motors.esp_magic(ESPvalue, motorFactors, trigger_left, trigger_right, last_data)

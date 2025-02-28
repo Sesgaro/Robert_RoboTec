50c734b754a437a7184aaef2ab48620b521ba550
Edgar Francisco Arechiga Nuño <122136427+Sesgaro@users.noreply.github.com>
2025-02-27 19:09:37 -0600

    Update Main_Control.py

--- Main_Control.py
+++ Main_Control.py
@@ -31,24 +31,23 @@ last_data = ''
 while True:
     pygame.event.pump()
 
-    new_trigger_left = motors.map_range(joystick.get_axis(4), -1, 1, 0, 250)
-    new_trigger_right = motors.map_range(joystick.get_axis(5), -1, 1, 0, 250)
+    new_trigger_left = motors.map_range(joystick.get_axis(4), -1, 1, 0, 100)
+    new_trigger_right = motors.map_range(joystick.get_axis(5), -1, 1, 0, 100)
     new_joy1_x = motors.map_range(joystick.get_axis(0), -1, 1, 0, 180)
     new_joy2_x = motors.map_range(joystick.get_axis(2), -1, 1, 0, 180)
-    
+
     new_trigger_left = (new_trigger_left // 5) * 5
     new_trigger_right = (new_trigger_right // 5) * 5
     new_joy1_x = (new_joy1_x // 5) * 5
     new_joy2_x = (new_joy2_x // 5) * 5
-    
+
     if (new_trigger_left != trigger_left or new_trigger_right != trigger_right or
         new_joy1_x != joy1_x or new_joy2_x != joy2_x):
         trigger_left = new_trigger_left
         trigger_right = new_trigger_right
         joy1_x = new_joy1_x
         joy2_x = new_joy2_x
-        
-        last_data = motors.esp_magic(ESPvalue, motorFactors, trigger_left, trigger_right, last_data)
-        print(f"Trigger Izq: {trigger_left} | Trigger Der: {trigger_right} | Joy1 X: {joy1_x} | Joy2 X: {joy2_x}")
-    
-    pygame.time.delay(100)
+
+        last_data = motors.esp_magic(ESPvalue, motorFactors, trigger_left, trigger_right, last_data)     
+        print(f"Trigger Izq: {trigger_left} | Trigger Der: {trigger_right} | Joy1 $
+

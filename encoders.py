dc7a29be26ff9f92b798db9564cbcc098ffe1f47
Edgar Francisco Arechiga Nuño <122136427+Sesgaro@users.noreply.github.com>
2025-02-27 19:15:11 -0600

    Update encoders.py

--- encoders.py
+++ encoders.py
@@ -1,7 +1,3 @@
 #Test 1
 import serial
 import time
-
-def map_range(value, in_min, in_max, out_min, out_max):
-    return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-

afd779d274e285772e525f937b6113080eef827f
Edgar Francisco Arechiga Nuño <122136427+Sesgaro@users.noreply.github.com>
2025-03-30 11:43:19 -0600

    Create Vesc_test.ino

---
+++ Arduino code/Vesc_test.ino
@@ -0,0 +1,45 @@
+#include <mcp_can.h>
+#include "vesc_can_bus_arduino.h"
+
+CAN can;             // get torque sensor data, throttle for now
+
+bool print_realtime_data = true;
+long last_print_data;
+
+
+void setup()
+{
+
+    Serial.begin(115200); // Wired comms from USB port
+
+    can.initialize();
+
+}
+
+void loop() {
+    can.spin();
+
+    if (millis() - last_print_data > 1000) {
+        Serial.print("rpm: ");
+        Serial.println(can.erpm);
+        Serial.print("voltage: ");
+        Serial.println(can.inpVoltage);
+        Serial.print("dutyCycleNow: ");
+        Serial.println(can.dutyCycleNow);
+        Serial.print("avgInputCurrent: ");
+        Serial.println(can.avgInputCurrent);
+        Serial.print("avgMotorCurrent: ");
+        Serial.println(can.avgMotorCurrent);
+        Serial.print("tempFET: ");
+        Serial.println(can.tempFET);
+        Serial.print("tempMotor: ");
+        Serial.println(can.tempMotor);
+        Serial.print("WattHours: ");
+        Serial.println(can.WattHours);
+        last_print_data = millis();
+        Serial.println("..............................");
+        Serial.println();
+    }
+    can.vesc_set_erpm(46);
+    delay(5);
+}
